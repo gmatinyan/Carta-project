8d20d17f37d467503d27ebae775228c2c28a5044
Gohar Matinyan <goharmatinyan@gmail.com>
2018-12-10 17:42:26 -0800

    saving work

---
+++ src/components/DatePicker.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import '../App.css';
+
+
+class DatePicker extends Component {
+	render() {
+		return (
+			<div className="datePicker-container">
+				<h2>DatePicker</h2>
+			</div>
+		);
+	}
+}
+
+export default DatePicker;
